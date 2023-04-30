449b7192e4c661f5b55c3e39c26ba17ab759b7bf
Mr.BrickCH@outlook.com <Mr.BrickCH@outlook.com>
2023-04-30 15:22:30 +0700

    Create LocationModel.kt and LocationService.kt for store and save info

---
+++ app/src/main/java/com/mr_brick/gps_tracker/location/LocationModel.kt
@@ -0,0 +1,9 @@
+package com.mr_brick.gps_tracker.location
+
+import org.osmdroid.util.GeoPoint
+
+data class LocationModel(
+    val velocity: Float = 0.0f,
+    val distance: Float = 0.0f,
+    val geoPointsList: ArrayList<GeoPoint>
+)
---
+++ app/src/main/java/com/mr_brick/gps_tracker/location/LocationService.kt
@@ -0,0 +1,25 @@
+package com.mr_brick.gps_tracker.location
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class LocationService : Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return START_STICKY
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+    }
+}
